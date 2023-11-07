0d3b54b490e557d2cd55feffcec7581e3e902dbf
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-11-07 13:45:10 +1000

    Define __init__ and __str__ methods for Product class

--- product.py
+++ product.py
@@ -1,3 +1,14 @@
 """Product class with Jason and Reece <3"""
 
+
 class Product:
+    """Represent a product object."""
+
+    def __init__(self, name, price):
+        """Initialise a product object."""
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        """Return string representation of product object."""
+        return f"{self.name}, ${self.price:.2f}"
